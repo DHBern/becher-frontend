6b35f51f46587c02450752520896a7d1f14afae9
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2023-11-27 11:46:47 +0100

    skeleton init

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+import { join } from 'path'
+
+import forms from '@tailwindcss/forms';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { becher } from './src/becher'
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		forms,
+		skeleton({
+			themes: {
+				preset: [
+					{
+						name: 'skeleton',
+						enhancements: true,
+					},
+				],
+				custom: [
+					becher,
+				],
+			},
+		}),
+	],
+};
