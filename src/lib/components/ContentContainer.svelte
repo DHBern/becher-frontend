bc86abba4e3b17c01ecfd01e738694ec21b24785
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2023-12-17 21:06:08 +0100

    add item-page

--- src/lib/components/ContentContainer.svelte
+++ src/lib/components/ContentContainer.svelte
@@ -5,11 +5,11 @@
 	export let dark = false;
 </script>
 
-<div class="w-full p-8 {dark ? 'bg-primary-500 text-secondary-500' : ''}">
+<section class="w-full p-8 {dark ? 'bg-primary-500 text-secondary-500' : ''}">
 	<div class="container mx-auto {className}">
 		<slot />
 	</div>
-</div>
+</section>
 
 <style lang="postcss">
 </style>
---
+++ src/routes/item/[slug]/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import ContentContainer from '$lib/components/ContentContainer.svelte';
+
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<ContentContainer>
+	image
+	<h1 class="h1">{data.id}</h1>
+	<dl>
+		<dt>Datierung</dt>
+		<dd>9999</dd>
+	</dl>
+</ContentContainer>
+<ContentContainer></ContentContainer>
