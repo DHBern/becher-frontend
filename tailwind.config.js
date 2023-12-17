cbe9ef43462d8a7e25c2e57c05dc62ad3dd117dd
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2023-12-17 18:41:10 +0100

    add container-query support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,31 @@
-import { join } from 'path'
+import { join } from 'path';
 
 import forms from '@tailwindcss/forms';
 import { skeleton } from '@skeletonlabs/tw-plugin';
-import { becher } from './src/becher'
+import { becher } from './src/becher';
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
 	theme: {
-		extend: {},
+		extend: {}
 	},
 	plugins: [
+		require('@tailwindcss/container-queries'),
 		forms,
 		skeleton({
 			themes: {
 				preset: [
 					{
 						name: 'skeleton',
-						enhancements: true,
-					},
+						enhancements: true
+					}
 				],
-				custom: [
-					becher,
-				],
-			},
-		}),
-	],
+				custom: [becher]
+			}
+		})
+	]
 };
