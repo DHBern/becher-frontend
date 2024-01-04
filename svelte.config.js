45b63cfce85dbcd6e6a8dfbed93f401f5ded1fa9
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-01-04 18:41:19 +0100

    add github basepath for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,10 @@ const config = {
 		adapter: adapter({
 			fallback: '404.html',
 			precompress: true
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 export default config;
