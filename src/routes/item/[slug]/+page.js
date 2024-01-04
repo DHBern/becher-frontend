c11c99fac372c0ffbdd085547577808d37b16abc
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-01-04 18:25:54 +0100

    add entries for statically rendering of dynamic route

--- src/routes/item/[slug]/+page.js
+++ src/routes/item/[slug]/+page.js
@@ -1,6 +1,13 @@
+import itemData from '../../items/compact.json';
+
 /** @type {import('./$types').PageLoad} */
 export function load({ params }) {
 	return {
 		id: params.slug
 	};
+}
+
+/** @type {import('./$types').EntryGenerator} */
+export function entries() {
+	return itemData.map((item) => ({ slug: item.signature }));
 }
