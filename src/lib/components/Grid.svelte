2e283a5d9ad699f793067d4743894ef551a4ff02
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-01-11 16:55:31 +0100

    apply grid-design

--- src/lib/components/Grid.svelte
+++ src/lib/components/Grid.svelte
@@ -5,15 +5,15 @@
 	export let items = [];
 </script>
 
-<div class="grid pt-8">
-	{#each items as item}
-		<article class="py-3">
+<div class="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4 pt-8">
+	{#each items as item (item.signature)}
+		<article class="hover:scale-125 transition-transform">
 			<a href="/item/{item.signature}">
 				<figure>
 					<div class="bg-{item.holding_institution === 'SLA' ? 'primary' : 'tertiary'}-500">
 						<enhanced:img src="$lib/assets/placeholder.jpg?w=300" class="mx-auto"></enhanced:img>
 					</div>
-					<figcaption>{item.title} [{item.date}]</figcaption>
+					<figcaption class="bg-surface-900">{item.title} [{item.date}]</figcaption>
 				</figure>
 			</a>
 		</article>
