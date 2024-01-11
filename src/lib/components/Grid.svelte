40bd4775bb5845c259ae9590c939678fc03ccc64
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-01-11 17:56:01 +0100

    use svelte-virtual-scroll-list

--- src/lib/components/Grid.svelte
+++ src/lib/components/Grid.svelte
@@ -1,4 +1,5 @@
 <script>
+	import VirtualScroll from 'svelte-virtual-scroll-list';
 	import { base } from '$app/paths';
 
 	/**
@@ -7,17 +8,25 @@
 	export let items = [];
 </script>
 
-<div class="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4 pt-8">
-	{#each items as item (item.signature)}
-		<article class="hover:scale-125 transition-transform">
-			<a href="{base}/item/{item.signature}">
-				<figure>
-					<div class="bg-{item.holding_institution === 'SLA' ? 'primary' : 'tertiary'}-500">
-						<enhanced:img src="$lib/assets/placeholder.jpg?w=300" class="mx-auto"></enhanced:img>
-					</div>
-					<figcaption class="bg-surface-900">{item.title} [{item.date}]</figcaption>
-				</figure>
-			</a>
-		</article>
-	{/each}
-</div>
+<VirtualScroll data={items} key="signature" let:data>
+	<article class="hover:scale-125 transition-transform">
+		<a href="{base}/item/{data.signature}">
+			<figure>
+				<div class="bg-{data.holding_institution === 'SLA' ? 'primary' : 'tertiary'}-500">
+					<enhanced:img
+						src="$lib/assets/placeholder.jpg?w=300"
+						class="mx-auto"
+						alt="digitales Faksimile"
+					></enhanced:img>
+				</div>
+				<figcaption class="bg-surface-900">{data.title} [{data.date}]</figcaption>
+			</figure>
+		</a>
+	</article>
+</VirtualScroll>
+
+<style>
+	:global(.virtual-scroll-wrapper) {
+		@apply grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4 pt-8;
+	}
+</style>
