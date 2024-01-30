1f4f0a4ae8ed840ad75830297ae14603372b6b28
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-01-30 21:18:00 +0100

    add DEA & SLA checkboxes to not forget to implement the function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -97,7 +97,7 @@
 
 			<h3 class="h3 m-3">Suchen in</h3>
 			<div class="text-sm grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 ml-6 sm:ml-0">
-				{#each data.itemstructure.map((i) => i.label) as text}
+				{#each [...data.itemstructure.map((i) => i.label), 'DEA', 'SLA'] as text}
 					<div>
 						<label class="flex items-center">
 							<input class="checkbox m-2 lg:m-5" type="checkbox" />
