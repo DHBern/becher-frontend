d0004cc967d1626dd9c8a744d774b81c60b36a83
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-03-18 14:21:43 +0100

    add bio content

--- src/routes/bio/+page.svelte
+++ src/routes/bio/+page.svelte
@@ -1,1 +1,62 @@
-<h1>bio page</h1>
+<script>
+	import ContentContainer from '$lib/components/ContentContainer.svelte';
+</script>
+
+<ContentContainer>
+	<h1 class="h1">Ulrich Becher</h1>
+	<blockquote class="blockquote">
+		Wir deutschsprechenden und -schreibenden Europäer im Exil sind wohloderübel ledig aller
+		weltpolitischen Berücksichtigung (bislang ist’s so, machen mir uns nichts vor!) und damit –
+		Rücksichtnahme; draus muss sich Verpflichtung ergeben zur Zeugnisablegung. - Ulrich Becher in
+		einem Brief an Balder Olden, 4. Juli 1943
+	</blockquote>
+	<p>
+		Als die Nationalsozialist*innen im Frühjahr 1933 die Macht in Deutschland übernahmen, stand
+		Ulrich Becher am Beginn einer vielversprechenden Karriere als Schriftsteller. Er war gerade 23
+		Jahre jung, als seinen Erinnerungen zufolge sein Erstling, der 1932 bei Rowohlt verlegte und
+		positiv besprochene Novellenband Männer machen Fehler, den NS-Bücherverbrennungen vom 10. Mai
+		1933 zum Opfer fiel. Zu diesem Zeitpunkt befand sich Becher schon außer Landes: „Hatte in der
+		Reichstagsbrandnacht 33 Deutschland verlassen, um, als dessen jüngster ‚entarteter Autor‘ und
+		jüngstes Mitglied des George Grosz-Kreises, der Gefahr der Schutzhaft zu entgehn, nicht aus
+		‚rassischen Gründen‘.“ Mit George Grosz, dessen Grafikschüler Becher seit 1927 war, verband ihn
+		eine lebenslange Freundschaft.
+	</p>
+
+	<p>
+		In den Jahren 1933 bis 1938 hält sich Becher vornehmlich in Österreich, aber auch in der Schweiz
+		auf. In Wien heiratet er im Herbst 1933 Dana Roda, die Tochter des österreichischen Humoristen
+		Alexander Roda Roda. Die geplante Berliner Uraufführung seines Stücks Niemand zerschlägt sich,
+		so dass das ‚neuzeitliche Mysterienspiel‘ erst 1936 im Stadttheater in Bern auf die Bühne kommt.
+		Der Erzählband Die Eroberer. Geschichten aus Europa erscheint 1936 mit einem vom Autor selbst
+		nicht anerkannten Geleitwort von Ernst Glaeser im Zürcher Oprecht Verlag.
+	</p>
+
+	<p>
+		Nach dem „Anschluss“ Österreichs emigrieren Ulrich und Dana Becher weiter in die Schweiz, ins
+		Engadin. Die Eindrücke dieser Zeit sind später in Bechers Hauptwerk, den großen Exilroman
+		Murmeljagd (1969) eingegangen. Obschon seine Mutter Elise Becher-Ulrich Schweizerin war, erhielt
+		der Autor keine dauerhafte Niederlassungsbewilligung. Dank eines tschechoslowakischen Passes
+		gelang Becher schließlich mit der Gruppe Goergen die Flucht aus Europa nach Brasilien. Nach drei
+		Jahren im brasilianischen Exil folgen vier weitere produktive Jahre in New York, ehe die Familie
+		Becher (1944 war der Sohn Martin zur Welt gekommen) nach Europa remigierte und sich ab 1954
+		dauerhaft in Basel niederließ. Becher lebte dort bis zu seinem Tod am 15. April 1990 in einem
+		Apartmenthaus.
+	</p>
+
+	<p>
+		Die Erfahrung des Exils und die kritische Auseinandersetzung mit den Katastrophen und Kriegen
+		des 20. Jahrhunderts als einer Epoche der Enthumanisierung bilden ein, wenn nicht das Kernthema
+		von Ulrich Bechers Schaffen: Stücke wie Der Bockerer und Samba, die New Yorker Novellen oder
+		Romane wie Kurz nach 4, William’s Ex-Casino und nicht zuletzt die Murmeljagd zeugen davon.
+	</p>
+
+	<p>
+		Quelle: <a
+			class="anchor"
+			target="_blank"
+			href="https://kuenste-im-exil.de/KIE/Content/DE/Personen/ulrich-becher.html"
+		>
+			Personenbeschreibung von Ulrich Becher bei Künste im Exil
+		</a>
+	</p>
+</ContentContainer>
