b3de93b800ecf8944bff5fb3aa05ea49c6177f29
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-06-06 22:18:33 +0200

    format about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,44 +4,84 @@
 
 <ContentContainer>
 	<h1 class="h1">Über das Projekt</h1>
-	Das binationale Kooperationsprojekt Ulrich Becher Digital soll den Zugang zum Nachlass des Exilschriftstellers
-	Ulrich Becher vereinfachen und ist eine Initiative des Deutschen Exilarchivs 1933-1945 der Deutschen
-	Nationalbibliothek und des Schweizerischen Literaturarchivs der Schweizerischen Nationalbibliothek.
-	Die technische Umsetzung erfolgt durch das Data Science Lab der Universität Bern. Lange Zeit galt Ulrich
-	Becher (1910-1990) als „vergessener“ Autor. Seit der Neuauflage seines Hauptwerks „Murmeljagd“ (1969)
-	im Schöffling Verlag 2009 wird sein Werk indes wiederentdeckt und zunehmend erforscht, wie beispielsweise
-	der aus dem Projekt hervorgegangene „text+kritik“-Band zu Ulrich Becher (2025) unterstreicht. Vor allem
-	Ulrich Bechers großer Roman „Murmeljagd“, in dem er die Flucht und das Exil in der ihm eigenen sprachmächtigen,
-	bildreichen und grotesken Art verarbeitet, erfuhr große Resonanz. Daneben wurden zuletzt auch seine
-	„New Yorker Novellen“ und „Das Herz des Hais“ bei Schöffling und Diogenes neu aufgelegt. Ulrich Bechers
-	Nachlass weist eine verwickelte, für Exilnachlässe indes durchaus typische Überlieferungsgeschichte
-	auf und wird heute sowohl im Deutschen Exilarchiv 1933-1945 (DEA) der Deutschen Nationalbibliothek
-	in Frankfurt als auch im Schweizerischen Literaturarchiv (SLA) der Schweizerischen Nationalbibliothek
-	in Bern jeweils als Teilnachlass aufbewahrt. In den Beständen beider Archive befinden sich Korrespondenzen,
-	Lebensdokumente, Manuskripte seiner Gedichte, Romane und Theaterstücke, Bilddokumente sowie Zeichnungen
-	Ulrich Bechers. Inhaltlich sind beide Teilnachlässe bis in die einzelnen Korrespondenzen und Fassungen
-	der Manuskripte hinein miteinander verwoben. Für die Forschung bedeutet dieser Umstand, dass Wissenschaftler*innen
-	beide Bestände konsultieren müssen, wenn sie fundierte Aussagen zu Bechers Biografie und Werk treffen
-	möchten. Das hohe Forschungsinteresse und der Wunsch nach nutzerfreundlicher Bereitstellung der beiden
-	Teilnachlässe führten zu einem gemeinsamen Projekt beider Archive. Aufbauend auf den bereits vorhandenen
-	Erschließungsergebnissen wurden die Teilbestände zwischen 2019-2021 tiefergehend nach dem internationalen
-	Regelwerk der „Ressourcenerschließung mit Normdaten in Archiven und Bibliotheken“ (RNAB) erschlossen.
-	Die granularere Erschließung zielte darauf ab, die Bestände perspektivisch digital zusammenzuführen
-	und für die Wissenschaft wie auch für die Öffentlichkeit besser zugänglich zu machen. Einen wichtigen
-	Meilenstein der Kooperation bildete die im virtuellen Museum Künste im Exil gemeinsam kuratierte Sonderausstellung
-	zu Ulrich Becher, die Mitte Mai 2021 freigeschaltet wurde. Die virtuelle Zusammenführung beider Teilnachlässe
-	basiert auf einer gemeinsamen Marktschau (2022) zu bislang umgesetzten technischen Beispiellösungen.
-	Als ein zentrales Ergebnis kristallisierte sich heraus, dass im Rahmen eines Vorprojektes ein Prototyp
-	realisiert werden sollte, um das Potential von Ulrich Becher digital anschaulich darzustellen. Folgende
-	Anforderungen sollten dabei im Fokus stehen. 1.Verknüpfung der normierten Metadaten aus den beiden
-	Teilnachlässen und Datenbanken 2.Modellierung und Auswertung der Metadaten in Form von georeferenzierten
-	und navigierbaren Karten- und/oder Netzwerkdarstellungen, die Entstehungsorte von Werken, Fluchtrouten
-	und Korrespondenzwege Bechers nachzeichnen 3.Präsentation von Digitalisaten aus sämtlichen Nachlassbereichen
-	und deren nutzerfreundliche Vermittlung 4.KI-basierte Visualisierungsoptionen (z.B. Ähnlichkeitssuche)
-	5.Citizen Science Tools (Transkription, Annotation) Parallel hierzu wurde die Digitalisierung beider
-	Teilnachlässe zwischen 2023 und 2024 realisiert. Mit dem hier vorliegenden Prototyp ist ein erster
-	Schritt getan, um eine differenzierte und effiziente Forschung zu Leben und Werk Ulrich Bechers zu
-	ermöglichen.
+	<p>
+		Das binationale Kooperationsprojekt Ulrich Becher Digital soll den Zugang zum Nachlass des
+		Exilschriftstellers Ulrich Becher vereinfachen und ist eine Initiative des Deutschen Exilarchivs
+		1933-1945 der Deutschen Nationalbibliothek und des Schweizerischen Literaturarchivs der
+		Schweizerischen Nationalbibliothek. Die technische Umsetzung erfolgt durch das Data Science Lab
+		der Universität Bern.
+	</p>
+
+	<p>
+		Lange Zeit galt Ulrich Becher (1910-1990) als „vergessener“ Autor. Seit der Neuauflage seines
+		Hauptwerks „Murmeljagd“ (1969) im Schöffling Verlag 2009 wird sein Werk indes wiederentdeckt und
+		zunehmend erforscht, wie beispielsweise der aus dem Projekt hervorgegangene „text+kritik“-Band
+		zu Ulrich Becher (2025) unterstreicht. Vor allem Ulrich Bechers großer Roman „Murmeljagd“, in
+		dem er die Flucht und das Exil in der ihm eigenen sprachmächtigen, bildreichen und grotesken Art
+		verarbeitet, erfuhr große Resonanz. Daneben wurden zuletzt auch seine „New Yorker Novellen“ und
+		„Das Herz des Hais“ bei Schöffling und Diogenes neu aufgelegt.
+	</p>
+
+	<p>
+		Ulrich Bechers Nachlass weist eine verwickelte, für Exilnachlässe indes durchaus typische
+		Überlieferungsgeschichte auf und wird heute sowohl im Deutschen Exilarchiv 1933-1945 (DEA) der
+		Deutschen Nationalbibliothek in Frankfurt als auch im Schweizerischen Literaturarchiv (SLA) der
+		Schweizerischen Nationalbibliothek in Bern jeweils als Teilnachlass aufbewahrt.
+	</p>
+
+	<p>
+		In den Beständen beider Archive befinden sich Korrespondenzen, Lebensdokumente, Manuskripte
+		seiner Gedichte, Romane und Theaterstücke, Bilddokumente sowie Zeichnungen Ulrich Bechers.
+		Inhaltlich sind beide Teilnachlässe bis in die einzelnen Korrespondenzen und Fassungen der
+		Manuskripte hinein miteinander verwoben. Für die Forschung bedeutet dieser Umstand, dass
+		Wissenschaftler*innen beide Bestände konsultieren müssen, wenn sie fundierte Aussagen zu Bechers
+		Biografie und Werk treffen möchten.
+	</p>
+
+	<p>
+		Das hohe Forschungsinteresse und der Wunsch nach nutzerfreundlicher Bereitstellung der beiden
+		Teilnachlässe führten zu einem gemeinsamen Projekt beider Archive. Aufbauend auf den bereits
+		vorhandenen Erschließungsergebnissen wurden die Teilbestände zwischen 2019-2021 tiefergehend
+		nach dem internationalen Regelwerk der „Ressourcenerschließung mit Normdaten in Archiven und
+		Bibliotheken“ (RNAB) erschlossen. Die granularere Erschließung zielte darauf ab, die Bestände
+		perspektivisch digital zusammenzuführen und für die Wissenschaft wie auch für die Öffentlichkeit
+		besser zugänglich zu machen.
+	</p>
+
+	<p>
+		Einen wichtigen Meilenstein der Kooperation bildete die im virtuellen Museum Künste im Exil
+		gemeinsam kuratierte Sonderausstellung zu Ulrich Becher, die Mitte Mai 2021 freigeschaltet
+		wurde.
+	</p>
+
+	<p>
+		Die virtuelle Zusammenführung beider Teilnachlässe basiert auf einer gemeinsamen Marktschau
+		(2022) zu bislang umgesetzten technischen Beispiellösungen. Als ein zentrales Ergebnis
+		kristallisierte sich heraus, dass im Rahmen eines Vorprojektes ein Prototyp realisiert werden
+		sollte, um das Potential von Ulrich Becher digital anschaulich darzustellen. Folgende
+		Anforderungen sollten dabei im Fokus stehen.
+	</p>
+
+	<ol>
+		<li>Verknüpfung der normierten Metadaten aus den beiden Teilnachlässen und Datenbanken</li>
+		<li>
+			Modellierung und Auswertung der Metadaten in Form von georeferenzierten und navigierbaren
+			Karten- und/oder Netzwerkdarstellungen, die Entstehungsorte von Werken, Fluchtrouten und
+			Korrespondenzwege Bechers nachzeichnen
+		</li>
+		<li>
+			Präsentation von Digitalisaten aus sämtlichen Nachlassbereichen und deren nutzerfreundliche
+			Vermittlung
+		</li>
+		<li>KI-basierte Visualisierungsoptionen (z.B. Ähnlichkeitssuche)</li>
+		<li>Citizen Science Tools (Transkription, Annotation)</li>
+	</ol>
+
+	<p>
+		Parallel hierzu wurde die Digitalisierung beider Teilnachlässe zwischen 2023 und 2024
+		realisiert. Mit dem hier vorliegenden Prototyp ist ein erster Schritt getan, um eine
+		differenzierte und effiziente Forschung zu Leben und Werk Ulrich Bechers zu ermöglichen.
+	</p>
 </ContentContainer>
 <ContentContainer>
 	<h1 class="h1">Impressum</h1>
