42c444c215aa603255042e5af381915be3101ed7
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2024-06-22 16:01:44 +0200

    add context

--- src/routes/related-tests/+page.svelte
+++ src/routes/related-tests/+page.svelte
@@ -13,10 +13,45 @@
 		const found = structuredClone(data.csv.find((row) => row.id.slice(1, -1) === search));
 		delete found.id;
 		results = found;
+		selected = itemData.find((item) => item.key === search);
 	}
-
+	let selected = {};
 	let results = {};
 </script>
+
+<ContentContainer>
+	<h1 class="h1">Testseite Dokumentähnlichkeit (inhaltsbasiert)</h1>
+	<p>Diese Seite dient zum Vergleich unterschiedlicher Methoden zur Ähnlichkeitsermittlung.</p>
+	<p>
+		Durch Eingabe einer ID/Signatur werden für 12 Vergleichsmethoden jeweils die fünf ähnlichsten
+		Items angezeigt.
+	</p>
+	<p>Zu beachten:</p>
+	<ul class="list-disc">
+		<li>
+			<p>
+				Zur Zeit liegen nur für DEA-Katalogisate Volltextdaten vor (OCR). Weil die Qualität in
+				vielen Fällen sehr dürftig ist, wurden mit Methoden zur Verarbeitung natürlicher Sprache
+				(NLP) sog. "Named Entities" extrahiert. Die Ähnlichkeitsermittlung verwendet diese
+				Textbestandteile (häufig handelt es sich um Personen- oder Ortsangaben).
+			</p>
+		</li>
+		<li>
+			<p>
+				Weil vorerst keine guten Volltextdaten vorliegen, wurden zur Ähnlichkeitsermittlung keine
+				Transformer-Modelle eingesetzt. Sie stellen aber eine vielversprechende Option für eine
+				spätere Umsetzung dar.
+			</p>
+		</li>
+		<li>
+			<p>
+				Denkbar wäre auch die additive Verwendung der hier getesten Ansätze, d.h. pro Dokument
+				würden alle Treffer zusammengefasst und nach Häufigkeit sortiert. Items, die durch mehrere
+				Methoden als ähnlich ermittelt werden, erhielten entsprechend mehr Gewicht.
+			</p>
+		</li>
+	</ul>
+</ContentContainer>
 
 <ContentContainer class="max-w-lg">
 	<form on:submit={doSearch}>
@@ -28,6 +63,12 @@
 	</form>
 </ContentContainer>
 
+{#if selected?.key}
+	<ContentContainer dark>
+		<h2 class="h2">Ausgewähltes Objekt</h2>
+		<OneRow items={[selected]} />
+	</ContentContainer>
+{/if}
 {#each Object.entries(results) as [key, value]}
 	{@const items = value
 		.replaceAll('"', '')
